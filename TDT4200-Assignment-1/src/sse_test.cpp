ad9a7234a6bf31431742a45c61f23417b3a5d91f
Pontius1007 <embrikeinang@msn.com>
2018-09-13 21:05:26 +0200

    Fucking work

--- TDT4200-Assignment-1/src/sse_test.cpp
+++ TDT4200-Assignment-1/src/sse_test.cpp
@@ -6,12 +6,12 @@ float randFloat() {
     return (float) (rand()) / (float) (RAND_MAX);
 }
 
-float4 randFloat4() {
-    float4 res;
-    res.x = randFloat();
-    res.y = randFloat();
-    res.z = randFloat();
-    res.w = randFloat();
+sse_float4 randFloat4() {
+    sse_float4 res;
+    res.elements[0] = randFloat();
+    res.elements[1] = randFloat();
+    res.elements[2] = randFloat();
+    res.elements[3] = randFloat();
     return res;
 }
 
@@ -19,18 +19,22 @@ void sse_test(Mesh &mesh) {
     //Not allowed to change:
     unsigned int const loadFactor = 1000;
     //
-    std::vector<float4> vertices;
-    std::vector<float4> rand1;
-    std::vector<float4> rand2;
-    std::vector<float4> rand3;
-    std::vector<float4> rand4;
+    std::vector<sse_float4> vertices;
+    std::vector<sse_float4> rand1;
+    std::vector<sse_float4> rand2;
+    std::vector<sse_float4> rand3;
+    std::vector<sse_float4> rand4;
     srand(time(NULL));
 
     vertices.resize(mesh.vertexCount);
 
     std::cout << "SSE_TEST: Initializing vectors... " << std::flush;
     for (unsigned int i=0; i < vertices.size(); i++) {
-        vertices[i] = mesh.vertices[i];
+        vertices[i].elements[0] = mesh.vertices[i].x;
+        vertices[i].elements[1] = mesh.vertices[i].y;
+        vertices[i].elements[2] = mesh.vertices[i].z;
+        vertices[i].elements[3] = mesh.vertices[i].w;
+
         rand1.push_back(randFloat4());
         rand2.push_back(randFloat4());
         rand3.push_back(randFloat4());
@@ -40,13 +44,33 @@ void sse_test(Mesh &mesh) {
     for (unsigned int j = 0; j < loadFactor; j++) {
         std::cout << "SSE_TEST: " << (j+1) << "/" << loadFactor << " Crunching numbers on " << vertices.size() << " vertices... " << "\r" << std::flush;
         for (unsigned int i=0; i < vertices.size(); i++) {
-            vertices[i] = vertices[i] + rand1[i];
-            vertices[i] = vertices[i] - rand2[i];
-            vertices[i] = vertices[i] * rand3[i];
-            if (rand4[i] != 0) {
-                vertices[i] = vertices[i] / rand4[i];
+
+            vertices[i].elements[0] = vertices[i].elements[0] + rand1[i].elements[0];
+            vertices[i].elements[1] = vertices[i].elements[1] + rand1[i].elements[1];
+            vertices[i].elements[2] = vertices[i].elements[2] + rand1[i].elements[2];
+            vertices[i].elements[3] = vertices[i].elements[3] + rand1[i].elements[3];
+
+
+            vertices[i].elements[0] = vertices[i].elements[0] - rand2[i].elements[0];
+            vertices[i].elements[1] = vertices[i].elements[1] - rand2[i].elements[1];
+            vertices[i].elements[2] = vertices[i].elements[2] - rand2[i].elements[2];
+            vertices[i].elements[3] = vertices[i].elements[3] - rand2[i].elements[3];
+
+
+            vertices[i].elements[0] = vertices[i].elements[0] * rand3[i].elements[0];
+            vertices[i].elements[1] = vertices[i].elements[1] * rand3[i].elements[1];
+            vertices[i].elements[2] = vertices[i].elements[2] * rand3[i].elements[2];
+            vertices[i].elements[3] = vertices[i].elements[3] * rand3[i].elements[3];
+
+            if (rand4[i].elements[0] != 0 || rand4[i].elements[1] != 0 || rand4[i].elements[2] != 0 || rand4[i].elements[3] != 0){
+                vertices[i].elements[0] = vertices[i].elements[0] / rand4[i].elements[0];
+                vertices[i].elements[1] = vertices[i].elements[1] / rand4[i].elements[1];
+                vertices[i].elements[2] = vertices[i].elements[2] / rand4[i].elements[2];
+                vertices[i].elements[3] = vertices[i].elements[3] / rand4[i].elements[3];
             }
         }
     }
     std::cout << std::endl;
 }
+
+
